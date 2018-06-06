6aa54650162252d1d310838aa44a884cbda9ef8f
Edward Nimocks <enimocks@gmail.com>
2018-06-06 16:56:13 -0400

    fix controller.js formatting

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,26 +10,25 @@ module.exports = function (app) {
     db.Burger.findAll({}).then(function (dbBurger) {
       res.json(dbBurger);
     });
-  )};
+  });
 
-    app.post("/api/burgers/", function (req, res) {
+  app.post("/api/burgers/", function (req, res) {
 
-      db.Burger.create({
-        burger_name: req.body.burger_name
-      }).then(function (dbBurger) {
-        res.json(dbBurger);
-      })
+    db.Burger.create({
+      burger_name: req.body.burger_name
+    }).then(function (dbBurger) {
+      res.json(dbBurger);
     });
+  });
 
-    app.put("/api/burgers/:id", function (req, res) {
-      burger.updateOne(req.params.id, function (result) {
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
+  app.put("/api/burgers/:id", function (req, res) {
+    burger.updateOne(req.params.id, function (result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
       }
-      );
+      res.status(200).end();
     });
+  });
 
-  }
+}
